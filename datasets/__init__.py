37ddda86bfa5d2f48a69765dc8da2e937f139440
SonPhatTranDeveloper <phat.tran.developer@gmail.com>
2024-01-02 10:44:56 +1100

    Update code

---
+++ datasets/__init__.py
@@ -0,0 +1,26 @@
+"""
+Generate a random linear regression dataset
+Author: Son Phat Tran
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+# Generate some random points X uniformly
+N = 20
+X = np.random.uniform(0, 10, N)
+X = np.sort(X)
+
+# True linear function
+m0 = 1
+m1 = 2
+
+# Generate label
+beta = 0.6
+t = m0 + m1 * X + np.random.normal(loc=0, scale=1/beta, size=N)
+
+
+if __name__ == "__main__":
+    # Plot the values
+    plt.scatter(X, t)
+    plt.show()
